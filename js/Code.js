de3dd39d2c0128842456a78fe333f8810713dcee
elizzie777 <85830044+elizzie777@users.noreply.github.com>
2021-12-17 11:24:48 +0000

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "KITTEN";
+const accessCode2 = "GAGGLE";
+const accessCode3 = "HUMMINGBIRD";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("T E K T I N", 100,50)
+    fill("purple")
+    text("Hint: a baby version of a cold animal", 100,70)
+
+fill("white")
+textSize(15)
+text("L G E G A G", 700,150)
+fill("purple")
+text("Hint: a group of geese", 700,170)
+
+fill("white")
+textSize(15)
+text("D H M N R I M G U B I", 100,250)
+fill("purple")
+text("Hint: the smallest animal !!", 100,270)
+
+}
